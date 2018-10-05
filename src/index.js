165084fd866786e29070b199ba4b1355534abdc1
unknown <thanhtra.le@ogilvy.com>
2018-10-06 00:22:37 +0700

    Attaching Redux store to React application

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
+import reducers from "./reducers";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 serviceWorker.unregister();
